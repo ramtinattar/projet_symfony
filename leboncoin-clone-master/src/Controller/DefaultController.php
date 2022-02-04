c9ec40c9a2b7f32b7095c64db59e9e912404e26a
Ramtin Attar <ramtinattar13@gmail.com>
2022-02-04 20:12:08 +0100

    push all

---
+++ leboncoin-clone-master/src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\City;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+    	$doc = $this->getDoctrine();
+
+        return $this->render('default/index.html.twig', [
+	        'cities' => $doc->getRepository(City::class)->findAll(),
+	        'categories' => $doc->getRepository(Category::class)->findAll()
+        ]);
+    }
+}
---
+++ leboncoin-clone-master/src/Repository/AdvertRepository.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Advert;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Advert|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Advert|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Advert[]    findAll()
+ * @method Advert[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AdvertRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Advert::class);
+    }
+
+	/**
+	 * Get all adverts of deal type
+	 *
+	 * @return Advert[]
+	 */
+	public function getAllDeals()
+	{
+		return $this->findByType('deal');
+    }
+
+	/**
+	 * Get all adverts of request type
+	 * @return Advert[]
+	 */
+	public function getAllRequests()
+	{
+		return $this->findByType('request');
+    }
+
+	/**
+	 * Find all adverts by type
+	 *
+	 * @param string $type
+	 *
+	 * @return Advert[]
+	 */
+	public function findByType( $type )
+	{
+		return $this->createQueryBuilder('a')
+		            ->andWhere('a.type = :val')
+		            ->setParameter('val', $type)
+		            ->orderBy('a.createdAt', 'DESC')
+		            ->getQuery()
+		            ->getResult();
+    }
+
+//    /**
+//     * @return Advert[] Returns an array of Advert objects
+//     */
+    
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    
+
+    
+    public function findOneBySomeField($value): ?Advert
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    
+}
